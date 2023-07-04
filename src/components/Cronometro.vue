b02969f5cfa7809f32bbbf68c0a1bb3e4d6eccc6
marbles60 <gitpessoal486@gmail.com>
2023-07-04 14:15:34 -0300

    Primeiro commit

---
+++ src/components/Cronometro.vue
@@ -0,0 +1,34 @@
+<template>
+    <section>
+        <strong class="display">
+            {{ tempoDecorrido }}
+        </strong>
+    </section>
+</template>
+
+<script lang="ts">
+// Obrigatório colocar o TS caso fizer um script em TypeScript
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'Cronometro',
+    props: {
+        tempoEmSegundos: {
+            type: Number,
+            default: 0
+        }
+    },
+    computed: {
+        tempoDecorrido(): string {
+            return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11, 8)
+        }
+    },
+})
+</script>
+
+<style>
+.display {
+    color: var(--texto-primario);
+
+}
+</style>
